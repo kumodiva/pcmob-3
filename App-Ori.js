36cbbd8ee6e42bb6cedca6ae2be764769e726137
kumodiva <kumodiva@gmail.com>
2022-07-02 08:40:20 +0800

    Incl AddColor function

--- App-Ori.js
+++ App-Ori.js
@@ -1,4 +1,3 @@
-
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
@@ -7,18 +6,18 @@ import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
 
 
- function HomeScreen() {
+function renderItem ({ item }) {
+  return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+}
+
+function HomeScreen() {
   const [colorArray, setColorArray] = useState([
     { red: 255, green: 0, blue: 0, id: "0" },
     { red: 0, green: 255, blue: 0, id: "1" },
     { red: 0, green: 0, blue: 255, id: "2" },
    ]);
 
- function renderItem({ item }) {
-   return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
- }
-
- function addColor() {
+function addColor() {
   setColorArray([
     ...colorArray,
     {
@@ -30,30 +29,19 @@ import BlockRGB from "./components/BlockRGB";
   ]);
 }
 
-return (
-  <View style={styles.container}>
-    <TouchableOpacity
-      style={{ height: 40, justifyContent: "center" }}
-      onPress={addColor}
-    >
-      <Text style={{ color: "red" }}>Add colour</Text>
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={{ height: 40, justifyContent: "center" }}
+        onPress={addColor}
+      >
+        <Text style={{ color: "red" }}>Add colour</Text>
       </TouchableOpacity>
-    <FlatList style={styles.list} data={colors} renderItem={renderItem} />
-  </View>
-);
 
-}
+      <FlatList style={styles.list} data={colors} renderItem={renderItem} />
+    </View>
+  );
 
-
-const Stack = createStackNavigator();
-export default function App() {
- return (
-   <NavigationContainer>
-     <Stack.Navigator>
-       <Stack.Screen name="Color List" component={HomeScreen} />
-     </Stack.Navigator>
-   </NavigationContainer>
- );
 }
 
 const styles = StyleSheet.create({
@@ -66,4 +54,17 @@ const styles = StyleSheet.create({
   list: {
     width: "100%",
   },
-});
+})
+
+const Stack = createStackNavigator();
+
+export default function App() {
+ return (
+   <NavigationContainer>
+     <Stack.Navigator>
+       <Stack.Screen name="Home" component={HomeScreen} />
+     </Stack.Navigator>
+   </NavigationContainer>
+ );
+}
+
