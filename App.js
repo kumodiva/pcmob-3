3bfdc9867fe6f509f28274635bda103d69a1e506
kumodiva <kumodiva@gmail.com>
2022-07-02 10:32:54 +0800

    Add RGB toucable

--- App.js
+++ App.js
@@ -1,68 +1,66 @@
 
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
 
-
- function HomeScreen() {
-  const [colorArray, setColorArray] = useState([
-    { red: 255, green: 0, blue: 0, id: "0" },
-    { red: 0, green: 255, blue: 0, id: "1" },
-    { red: 0, green: 0, blue: 255, id: "2" },
-   ]);
+function HomeScreen({ navigation }) {
+ const [colorArray, setColorArray] = useState([]);
 
  function renderItem({ item }) {
-   return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
+   return (
+     <TouchableOpacity>
+       <BlockRGB red={item.red} green={item.green} blue={item.blue} />
+     </TouchableOpacity>
+   );
  }
-
  function resetColors() {
   setColorArray([ ]);
 }
-
  function addColor() {
-  setColorArray([
-    {
-      red: Math.floor(Math.random() * 256),
-      green: Math.floor(Math.random() * 256),
-      blue: Math.floor(Math.random() * 256),
-      id: `${colorArray.length}`,
-    },
-    ...colorArray,
+   setColorArray([
+     ...colorArray,
+     {
+       red: Math.floor(Math.random() * 256),
+       green: Math.floor(Math.random() * 256),
+       blue: Math.floor(Math.random() * 256),
+       id: `${colorArray.length}`,
+     },
+   ]);
+ }
 
-  ]);
-}
-
-return (
-  <View style={styles.container}>
-    <TouchableOpacity
-      style={{ height: 40, justifyContent: "center" }}
-      onPress={addColor}
-    >
-      <Text style={{ color: "red" }}>Add colour</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity
+ return (
+   <View style={styles.container}>
+     <TouchableOpacity
+       style={{ height: 40, justifyContent: "center" }}
+       onPress={addColor}
+     >
+       <Text style={{ color: "red" }}>Add colour</Text>
+     </TouchableOpacity>
+     <TouchableOpacity
       style={{ height: 40, justifyContent: "center" }}
       onPress={resetColors}
     >
       <Text style={{ color: "red" }}>Reset colour</Text>
       </TouchableOpacity>
-    <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
-  </View>
-);
-
+     <FlatList style={styles.list} data={colorArray} renderItem={renderItem} />
+   </View>
+ );
 }
 
+function DetailsScreen() {
+  return<Text>Details Screen</Text>
+}
 
 const Stack = createStackNavigator();
+
 export default function App() {
  return (
    <NavigationContainer>
      <Stack.Navigator>
-       <Stack.Screen name="Color List" component={HomeScreen} />
+       <Stack.Screen name="Colour List" component={HomeScreen} />
        <Stack.Screen name="Details" component={DetailsScreen} />
      </Stack.Navigator>
    </NavigationContainer>
@@ -70,13 +68,17 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  list: {
-    width: "100%",
-  },
+ container: {
+   flex: 1,
+   backgroundColor: "#fff",
+   alignItems: "center",
+   justifyContent: "center",
+ },
+ list: {
+   width: "100%",
+ },
+ detailText: {
+   fontSize: 24,
+   marginBottom: 20,
+ },
 });
